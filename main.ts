00f203874aaddc37e4d43d59b67afba2eedf553b
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2023-11-30 11:48:23 +0100

    APP

--- main.ts
+++ main.ts
@@ -1,8 +1,61 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import express from "npm:express@4.18.2";
+import mongoose from "npm:mongoose@7.6.3";
+
+/*
+import addCliente from "./resolvers/addCliente.ts";
+import addGestor from "./resolvers/addGestor.ts";
+import addHipoteca from "./resolvers/addHipoteca.ts";
+import deleteCliente from "./resolvers/deleteCliente.ts";
+import asignarGestorCliente from "./resolvers/asignarGestorCliente.ts";
+import amortizarHipotecaCliente from "./resolvers/amortizarHipotecaCliente.ts";
+import transaccionParaCliente from "./resolvers/transaccionParaCliente.ts";
+import ingresarDineroCliente from "./resolvers/ingresarDineroCliente.ts";
+import depositDineroClientes from "./tasks/depositDineroClientes.ts";
+import payingCoutasHipotecas from "./tasks/payingCoutasHipotecas.ts";
+*/
+
+// Importación de función 'load' de Deno para cargar variables de entorno.
+import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts"; // Leer variables de entorno
+const env = await load(); // Carga Variables de entorno
+
+// Variables de entorno.
+const MONGO_URL = env.MONGO_URL || Deno.env.get("MONGO_URL") || "mongodb+srv://rmontenegrop:Lllubo6BT2sVncJg@clusteruni.pagju8q.mongodb.net/BaseDatosEmpresaNebrija?retryWrites=true&w=majority";
+const PORT = env.PORT || Deno.env.get("PORT") || 3040;
+
+// Verifica variables de entorno.
+if (!MONGO_URL || !PORT) {
+  console.log("No mongo URL or Port found");
+  Deno.exit(1);
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+// Conexión a la base de datos MongoDB.
+try{
+  await mongoose.connect(MONGO_URL);
+  console.info("Mongo Connected");
+}catch(e){
+  console.error(e);
 }
+
+// Creación de una instancia de Express.
+const app = express();
+app.use(express.json());
+
+// Rutas y controladores.
+/*
+app
+  .post("/api/BancoNebrija/addCliente", addCliente)
+  .post("/api/BancoNebrija/addGestor", addGestor)
+  .post("/api/BancoNebrija/addHipoteca", addHipoteca)
+  .delete("/api/BancoNebrija/deleteCliente/:id", deleteCliente)
+  .put("/api/BancoNebrija/asignarGestorCliente/:id", asignarGestorCliente)
+  .put("/api/BancoNebrija/amortizarHipotecaCliente/:id", amortizarHipotecaCliente)
+  .put("/api/BancoNebrija/transaccionParaCliente/:id", transaccionParaCliente)
+  .put("/api/BancoNebrija/ingresarDineroCliente/:id", ingresarDineroCliente);
+*/
+// Ejecutar las funciones cada 5 minutos
+const tiempoEjecucion = 5 * 60 * 1000; // 5 minutos en milisegundos
+
+// Iniciar el servidor.
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
