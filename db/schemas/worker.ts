10a5b6bdf9c704abdd4dac66d2e952d64ffbba2c
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2023-12-07 05:07:36 +0100

    APP

--- db/schemas/worker.ts
+++ db/schemas/worker.ts
@@ -32,6 +32,64 @@ WorkerSchema.post(
 );
 WorkerSchema.post(["findOneAndDelete"], WorkerPostDelete);
 
+// Valida si el correo electrónico tiene el formato *@*.*
+WorkerSchema.path("email").validate(async function (
+  email: string,
+) {
+  // Verifica si ha cambiado el email
+  if (email === this.email) {
+    return true;
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(!emailRegex.test(email)){
+    throw new Error(`Email has to be like this: \"name@domain.es\" or \"name123@business.com\"`);
+  }
+  return true;
+});
+
+// Validar que todas las tareas con los IDs dados existen
+WorkerSchema.path("tasksIDs").validate(async function (
+  tasksIDs: mongoose.Types.ObjectId[],
+) {
+  // Verifica si ha cambiado las tasksIDs
+  if (tasksIDs === this.tasksIDs) {
+    return true;
+  }
+  try {
+    const tasksCount = await mongoose.models.Task.countDocuments({
+      _id: { $in: tasksIDs },
+    });
+    if (tasksCount !== tasksIDs.length) {
+      throw new Error(`Tasks id founded ${tasksCount}, some Tasks not exists`);
+    }
+    if (tasksCount > 10) {
+      throw new Error("Worker can only have a maximum of 10 taks");
+    }
+    return true;
+  } catch (_e) {
+    return false;
+  }
+});
+
+// Validar que el businessID exists
+WorkerSchema.path("businessID").validate(async function (
+  value: mongoose.Types.ObjectId,
+) {
+  // Verifica si ha cambiado el businessID
+  if (value === this.businessID) {
+    return true;
+  }
+  try {
+    const business = await mongoose.models.Business.findById(value);
+    if (!business) {
+      throw new Error(`Business with id ${value} does not exist`);
+    }
+    return true;
+  } catch (_e) {
+    return false;
+  }
+});
+
 export const WorkerModel = mongoose.model<WorkerModelType>(
   "Worker",
   WorkerSchema,
